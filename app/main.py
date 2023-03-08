7e4154861eecfad5570c777f9c3a67370e61d3fb
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-03-07 23:39:08 -0600

    Remove file after fileresponse is sent

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
+from os import remove
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.background import BackgroundTasks
+
 from app.qr_codes.qr_generator import QrGenerator
 from app.schemas.create_qr import CreateQR
 
@@ -12,9 +16,12 @@ qr_generator = QrGenerator("app/static/qrcodes")
 
 @app.post("/qr/", status_code=201)
 async def generate_qr_code(
-    create_qr: CreateQR
+    create_qr: CreateQR,
+    background_job: BackgroundTasks
 ):
     file_path = qr_generator.generate_qr_code(
-        create_qr.url, create_qr.dark_color, create_qr.light_color, create_qr.size)
+        create_qr.url, create_qr.dark_color, create_qr.light_color, create_qr.scale)
+
+    background_job.add_task(remove, file_path)
 
     return FileResponse(file_path, status_code=201)
